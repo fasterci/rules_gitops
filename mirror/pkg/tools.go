c380ab3897451805b320ffac69dd346d05864910
Aleksey Pesternikov <apesternikov@gmail.com>
2024-06-12 18:35:09 -0700

    package names for crane and registry

--- mirror/pkg/tools.go
+++ mirror/pkg/tools.go
@@ -3,7 +3,7 @@
 
 package pkg
 
-// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// https://go.dev/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 
 import (
 	_ "github.com/google/go-containerregistry/cmd/crane"
