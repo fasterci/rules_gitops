14aabb78644460cae00061231e01edd79ba8a019
Aleksey Pesternikov <apesternikov@gmail.com>
2024-08-30 18:58:40 -0700

    address review

--- mirror/validate_image.sh
+++ mirror/validate_image.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 REMOTE="{src_image}"
 LOCAL="{dst_image}@{digest}"
-echo "Validating images $REMOTE and $LOCAL"
-{crane_tool} validate -v --fast --remote $REMOTE || {crane_tool} validate -v --fast --remote $LOCAL || exit 1
+echo "Validating images ${REMOTE} and ${LOCAL}"
+{crane_tool} validate -v --fast --remote ${REMOTE}
+if [ $? -eq 0 ]; then
+  echo "Image ${REMOTE} exist"
+  exit 0
+fi
+
+echo "Image ${REMOTE} does not exist, checking ${LOCAL}"
+
+{crane_tool} validate -v --fast --remote ${LOCAL}
+if [ $? -eq 0 ]; then
+  echo "Image ${LOCAL} exist"
+  exit 0
+fi
+
+echo "Image ${LOCAL} does not exist"
+exit 1
