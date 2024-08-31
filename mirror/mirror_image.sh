d358aba6d77ce74b25d0d7b55a8ac77c30f6874d
apesternikov <apesternikov@gmail.com>
2024-08-30 22:32:21 -0700

    mirror validation should pass with local image only; more testing for mirroring (#47)
    * mirror validation should pass with local image only; more testing for mirror
    
    * address review

--- mirror/mirror_image.sh
+++ mirror/mirror_image.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 set -eu
 
 function guess_runfiles() {
--- mirror/validate_image.sh
+++ mirror/validate_image.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-echo "Validating image $1"
-$CRANE_BIN validate -v --fast --remote $1
+REMOTE="{src_image}"
+LOCAL="{dst_image}@{digest}"
+echo "Validating images ${REMOTE} and ${LOCAL}"
+{crane_tool} validate -v --fast --remote ${REMOTE}
+if [ $? -eq 0 ]; then
+  echo "Image ${REMOTE} exist"
+  exit 0
+fi
+
+echo "Image ${REMOTE} does not exist, checking ${LOCAL}"
+
+{crane_tool} validate -v --fast --remote ${LOCAL}
+if [ $? -eq 0 ]; then
+  echo "Image ${LOCAL} exist"
+  exit 0
+fi
+
+echo "Image ${LOCAL} does not exist"
+exit 1
