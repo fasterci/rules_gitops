4f7723ed22a9b266c7000e2308d8c22215cac20c
Aleksey Pesternikov <apesternikov@gmail.com>
2026-06-02 16:59:56 -0700

    keep it v7 compatible

--- gitops/prer/create_gitops_prs.go
+++ gitops/prer/create_gitops_prs.go
@@ -106,7 +106,7 @@ func cleanTarget(target string) string {
 
 func bazelQueryTargets(query string) []GitopsTarget {
 	log.Println("Executing bazel cquery ", query)
-	starlarkExpr := `json.encode(struct(deployment_branch = getattr(providers(target)['//gitops:provider.bzl%GitopsArtifactsInfo'], 'deployment_branch', '') if '//gitops:provider.bzl%GitopsArtifactsInfo' in providers(target) else '', target = str(target.label), executable = providers(target)['DefaultInfo'].files_to_run.executable.path if providers(target)['DefaultInfo'].files_to_run.executable else ''))`
+	starlarkExpr := `json.encode(struct(deployment_branch = getattr(providers(target)['//gitops:provider.bzl%GitopsArtifactsInfo'], 'deployment_branch', '') if '//gitops:provider.bzl%GitopsArtifactsInfo' in providers(target) else '', target = str(target.label), executable = target.files_to_run.executable.path if target.files_to_run.executable else ''))`
 	cmd := oe.Command(*bazelCmd, "cquery", "--implicit_deps=false", query, "--output=starlark", "--starlark:expr="+starlarkExpr)
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
